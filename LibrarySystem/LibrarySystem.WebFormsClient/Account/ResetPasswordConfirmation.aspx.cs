becfe33596758e3c9a362f525ee5dcecc847ce39
Daniel Penchev <petariwwanoww@gmail.com>
2017-05-01 23:57:55 +0300

    WebClient project added

---
+++ LibrarySystem/LibrarySystem.WebFormsClient/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace LibrarySystem.WebFormsClient.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ LibrarySystem/LibrarySystem.WebFormsClient/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LibrarySystem.WebFormsClient.Startup))]
+namespace LibrarySystem.WebFormsClient
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
