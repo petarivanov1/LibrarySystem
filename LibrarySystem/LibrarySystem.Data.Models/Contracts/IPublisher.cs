39271a371be401337b265a2574f1720b2436f14e
Daniel Penchev <petariwwanoww@gmail.com>
2017-05-01 09:53:02 +0300

    Added class Publisher and interface IPublisher

---
+++ LibrarySystem/LibrarySystem.Data.Models/Contracts/IPublisher.cs
@@ -0,0 +1,9 @@
+﻿namespace LibrarySystem.Data.Models.Contracts
+{
+    public interface IPublisher
+    {
+        int Id { get; set; }
+
+        string Name { get; set; }
+    }
+}
---
+++ LibrarySystem/LibrarySystem.Data.Models/Models/Publisher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using LibrarySystem.Data.Models.Contracts;
+
+namespace LibrarySystem.Data.Models.Models
+{
+    public class Publisher : IPublisher
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
